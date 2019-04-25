7011d0085d29eda1c1d84c15392813382f3f6bb8
andy-len <794578956@qq.com>
2019-04-25 17:36:49 +0800

    update-添加elementUI

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import ElementUI from 'element-ui';
 // import iView from "iview";
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 // Vue.use(iView, {
 //   i18n: (path, options) => {
 //     let value = i18n.t(path, options);
